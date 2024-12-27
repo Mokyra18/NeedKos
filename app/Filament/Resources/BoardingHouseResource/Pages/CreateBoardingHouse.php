ead445cc47c039e6dc19bd07bbb6d877bf2e71e3
Mokyra18 <rizkyramadhani181102@gmail.com>
2024-12-27 20:37:38 +0700

    first commit

---
+++ app/Filament/Resources/BoardingHouseResource/Pages/CreateBoardingHouse.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\BoardingHouseResource\Pages;
+
+use App\Filament\Resources\BoardingHouseResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBoardingHouse extends CreateRecord
+{
+    protected static string $resource = BoardingHouseResource::class;
+
+}
