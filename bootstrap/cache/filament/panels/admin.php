5c1c84dadba490be732750cec1c134a8a7784a25
Mokyra18 <rizkyramadhani181102@gmail.com>
2024-12-27 21:50:41 +0700

    first commit

---
+++ bootstrap/cache/filament/panels/admin.php
@@ -0,0 +1,79 @@
+<?php return array (
+  'livewireComponents' => 
+  array (
+    'app.filament.resources.boarding-house-resource.pages.create-boarding-house' => 'App\\Filament\\Resources\\BoardingHouseResource\\Pages\\CreateBoardingHouse',
+    'app.filament.resources.boarding-house-resource.pages.edit-boarding-house' => 'App\\Filament\\Resources\\BoardingHouseResource\\Pages\\EditBoardingHouse',
+    'app.filament.resources.boarding-house-resource.pages.list-boarding-houses' => 'App\\Filament\\Resources\\BoardingHouseResource\\Pages\\ListBoardingHouses',
+    'app.filament.resources.category-resource.pages.create-category' => 'App\\Filament\\Resources\\CategoryResource\\Pages\\CreateCategory',
+    'app.filament.resources.category-resource.pages.edit-category' => 'App\\Filament\\Resources\\CategoryResource\\Pages\\EditCategory',
+    'app.filament.resources.category-resource.pages.list-categories' => 'App\\Filament\\Resources\\CategoryResource\\Pages\\ListCategories',
+    'app.filament.resources.city-resource.pages.create-city' => 'App\\Filament\\Resources\\CityResource\\Pages\\CreateCity',
+    'app.filament.resources.city-resource.pages.edit-city' => 'App\\Filament\\Resources\\CityResource\\Pages\\EditCity',
+    'app.filament.resources.city-resource.pages.list-cities' => 'App\\Filament\\Resources\\CityResource\\Pages\\ListCities',
+    'app.filament.resources.testimonial-resource.pages.create-testimonial' => 'App\\Filament\\Resources\\TestimonialResource\\Pages\\CreateTestimonial',
+    'app.filament.resources.testimonial-resource.pages.edit-testimonial' => 'App\\Filament\\Resources\\TestimonialResource\\Pages\\EditTestimonial',
+    'app.filament.resources.testimonial-resource.pages.list-testimonials' => 'App\\Filament\\Resources\\TestimonialResource\\Pages\\ListTestimonials',
+    'app.filament.resources.transaction-resource.pages.create-transaction' => 'App\\Filament\\Resources\\TransactionResource\\Pages\\CreateTransaction',
+    'app.filament.resources.transaction-resource.pages.edit-transaction' => 'App\\Filament\\Resources\\TransactionResource\\Pages\\EditTransaction',
+    'app.filament.resources.transaction-resource.pages.list-transactions' => 'App\\Filament\\Resources\\TransactionResource\\Pages\\ListTransactions',
+    'filament.pages.dashboard' => 'Filament\\Pages\\Dashboard',
+    'filament.widgets.account-widget' => 'Filament\\Widgets\\AccountWidget',
+    'filament.livewire.database-notifications' => 'Filament\\Livewire\\DatabaseNotifications',
+    'filament.pages.auth.edit-profile' => 'Filament\\Pages\\Auth\\EditProfile',
+    'filament.livewire.global-search' => 'Filament\\Livewire\\GlobalSearch',
+    'filament.livewire.notifications' => 'Filament\\Livewire\\Notifications',
+    'filament.pages.auth.login' => 'Filament\\Pages\\Auth\\Login',
+  ),
+  'clusters' => 
+  array (
+  ),
+  'clusteredComponents' => 
+  array (
+  ),
+  'clusterDirectories' => 
+  array (
+  ),
+  'clusterNamespaces' => 
+  array (
+  ),
+  'pages' => 
+  array (
+    0 => 'Filament\\Pages\\Dashboard',
+  ),
+  'pageDirectories' => 
+  array (
+    0 => 'C:\\Users\\KULIAH\\SEMESTER 7\\Kerja Praktek\\NeedKos\\app\\Filament/Pages',
+  ),
+  'pageNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Pages',
+  ),
+  'resources' => 
+  array (
+    'C:\\Users\\KULIAH\\SEMESTER 7\\Kerja Praktek\\NeedKos\\app\\Filament\\Resources\\BoardingHouseResource.php' => 'App\\Filament\\Resources\\BoardingHouseResource',
+    'C:\\Users\\KULIAH\\SEMESTER 7\\Kerja Praktek\\NeedKos\\app\\Filament\\Resources\\CategoryResource.php' => 'App\\Filament\\Resources\\CategoryResource',
+    'C:\\Users\\KULIAH\\SEMESTER 7\\Kerja Praktek\\NeedKos\\app\\Filament\\Resources\\CityResource.php' => 'App\\Filament\\Resources\\CityResource',
+    'C:\\Users\\KULIAH\\SEMESTER 7\\Kerja Praktek\\NeedKos\\app\\Filament\\Resources\\TestimonialResource.php' => 'App\\Filament\\Resources\\TestimonialResource',
+    'C:\\Users\\KULIAH\\SEMESTER 7\\Kerja Praktek\\NeedKos\\app\\Filament\\Resources\\TransactionResource.php' => 'App\\Filament\\Resources\\TransactionResource',
+  ),
+  'resourceDirectories' => 
+  array (
+    0 => 'C:\\Users\\KULIAH\\SEMESTER 7\\Kerja Praktek\\NeedKos\\app\\Filament/Resources',
+  ),
+  'resourceNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Resources',
+  ),
+  'widgets' => 
+  array (
+    0 => 'Filament\\Widgets\\AccountWidget',
+  ),
+  'widgetDirectories' => 
+  array (
+    0 => 'C:\\Users\\KULIAH\\SEMESTER 7\\Kerja Praktek\\NeedKos\\app\\Filament/Widgets',
+  ),
+  'widgetNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Widgets',
+  ),
+);
